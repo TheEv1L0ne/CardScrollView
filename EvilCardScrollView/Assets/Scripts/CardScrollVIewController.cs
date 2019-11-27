1b16b501265cabaf81ed86bb1a620e9d40ebe90b
TheEv1L0ne <marko.leskovsek.91@gmail.com>
2019-11-27 23:51:33 +0100

    Base class is now inherited from EventTrigger and add base events

--- EvilCardScrollView/Assets/Scripts/CardScrollVIewController.cs
+++ EvilCardScrollView/Assets/Scripts/CardScrollVIewController.cs
@@ -1,9 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class CardScrollVIewController : MonoBehaviour
+public class CardScrollVIewController : EventTrigger
 {
+    public override void OnBeginDrag(PointerEventData eventData)
+    {
+        base.OnBeginDrag(eventData);
+        Debug.Log($"OnBeginDrag");
+    }
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        base.OnDrag(eventData);
+        Debug.Log($"OnDrag");
+    }
+
+    public override void OnEndDrag(PointerEventData eventData)
+    {
+        base.OnEndDrag(eventData);
+        Debug.Log($"OnEndDrag");
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +36,6 @@ public class CardScrollVIewController : MonoBehaviour
     {
         
     }
+
+
 }
