858ad400e5cc89a0681e0257d0d002cbe135d3d2
TheEv1L0ne <marko.leskovsek.91@gmail.com>
2019-12-14 19:03:06 +0100

    Now requires CardControllerEvents component

--- EvilCardScrollView/Assets/Scripts/CardControllerComponents.cs
+++ EvilCardScrollView/Assets/Scripts/CardControllerComponents.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(CardControllerEvents))]
 public class CardControllerComponents : MonoBehaviour
 {
     [SerializeField]
